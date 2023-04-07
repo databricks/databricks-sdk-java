cb6e2dd9953a81895b01984f36c2e4015151d6f8
Serge Smertin <259697+nfx@users.noreply.github.com>
2023-04-07 16:13:40 +0200

    Ported node type and spark version selectors from Terraform provider (#12)

--- databricks-sdk-java/src/test/java/com/databricks/sdk/integration/ClustersIT.java
+++ databricks-sdk-java/src/test/java/com/databricks/sdk/integration/ClustersIT.java
@@ -4,6 +4,9 @@ import com.databricks.sdk.DatabricksWorkspace;
 import com.databricks.sdk.integration.framework.EnvContext;
 import com.databricks.sdk.integration.framework.EnvOrSkip;
 import com.databricks.sdk.integration.framework.EnvTest;
+import com.databricks.sdk.mixin.ClustersExt;
+import com.databricks.sdk.mixin.NodeTypeSelector;
+import com.databricks.sdk.mixin.SparkVersionSelector;
 import com.databricks.sdk.service.clusters.GetEvents;
 import com.databricks.sdk.service.clusters.GetEventsResponse;
 import org.junit.jupiter.api.Test;
@@ -18,5 +21,23 @@ public class ClustersIT {
     void listsEvents(DatabricksWorkspace w, @EnvOrSkip("TEST_DEFAULT_CLUSTER_ID") String clusterId) {
         GetEventsResponse events = w.clusters().events(new GetEvents().setClusterId(clusterId));
         assertFalse(events.getEvents().isEmpty());
+    }
+
+    @Test
+    void smallestNode(DatabricksWorkspace w) {
+        ClustersExt clustersExt = new ClustersExt(w.getApiClient());
+
+        String nodeType = clustersExt.selectNodeType(new NodeTypeSelector().withLocalDisk());
+
+        assertNotNull(nodeType);
+    }
+
+    @Test
+    void latestRuntime(DatabricksWorkspace w) {
+        ClustersExt clustersExt = new ClustersExt(w.getApiClient());
+
+        String runtime = clustersExt.selectSparkVersion(new SparkVersionSelector().withLatest());
+
+        assertNotNull(runtime);
     }
 }
