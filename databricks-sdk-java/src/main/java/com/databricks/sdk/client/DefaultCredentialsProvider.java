e81c92428e94d98700eb8e8f61417e90c46ddd6f
Miles Yucht <miles@databricks.com>
2023-04-12 16:41:44 +0200

    Add to DefaultCredentialsProvider

--- databricks-sdk-java/src/main/java/com/databricks/sdk/client/DefaultCredentialsProvider.java
+++ databricks-sdk-java/src/main/java/com/databricks/sdk/client/DefaultCredentialsProvider.java
@@ -1,33 +1,41 @@
 package com.databricks.sdk.client;
 
+import com.databricks.sdk.client.oauth.AzureServicePrincipalCredentialsProvider;
+import com.databricks.sdk.client.oauth.OAuthM2MServicePrincipalCredentialsProvider;
+
 import java.util.Arrays;
 import java.util.List;
 
 public class DefaultCredentialsProvider implements CredentialsProvider {
-  private static List<CredentialsProvider> providers =
-      Arrays.asList(new PatCredentialsProvider(), new BasicCredentialsProvider());
+    private static List<CredentialsProvider> providers =
+            Arrays.asList(
+                    new PatCredentialsProvider(),
+                    new BasicCredentialsProvider(),
+                    new OAuthM2MServicePrincipalCredentialsProvider(),
+                    new AzureServicePrincipalCredentialsProvider()
+            );
 
-  private String authType = "default";
+    private String authType = "default";
 
-  @Override
-  public synchronized HeaderFactory configure(DatabricksConfig config) {
-    for (CredentialsProvider provider : providers) {
-      if (config.getAuthType() != null
-          && !config.getAuthType().isEmpty()
-          && provider.authType() != config.getAuthType()) {
-        continue;
-      }
-      HeaderFactory headerFactory = provider.configure(config);
-      if (headerFactory == null) {
-        continue;
-      }
-      authType = provider.authType();
-      return headerFactory;
+    @Override
+    public synchronized HeaderFactory configure(DatabricksConfig config) throws Exception {
+        for (CredentialsProvider provider : providers) {
+            if (config.getAuthType() != null
+                    && !config.getAuthType().isEmpty()
+                    && provider.authType() != config.getAuthType()) {
+                continue;
+            }
+            HeaderFactory headerFactory = provider.configure(config);
+            if (headerFactory == null) {
+                continue;
+            }
+            authType = provider.authType();
+            return headerFactory;
+        }
+        throw new DatabricksException("cannot configure default credentials");
     }
-    throw new DatabricksException("cannot configure default credentials");
-  }
 
-  public String authType() {
-    return authType;
-  }
+    public String authType() {
+        return authType;
+    }
 }
