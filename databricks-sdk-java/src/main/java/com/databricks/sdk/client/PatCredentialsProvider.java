eaf2ddd5084a04e21ba7aef0b1e6b0282777afcc
Tanmay Rustagi <tanmay.rustagi@databricks.com>
2023-04-13 03:42:20 +0200

    fmt

--- databricks-sdk-java/src/main/java/com/databricks/sdk/client/PatCredentialsProvider.java
+++ databricks-sdk-java/src/main/java/com/databricks/sdk/client/PatCredentialsProvider.java
@@ -13,7 +13,8 @@ public class PatCredentialsProvider implements CredentialsProvider {
 
   @Override
   public HeaderFactory configure(DatabricksConfig config) {
-    // tanmaytodo -- might actually need innerconfig because config shouldn't have anything set (according to python sdk) in some tests.
+    // tanmaytodo -- might actually need innerconfig because config shouldn't have anything set
+    // (according to python sdk) in some tests.
     // This is causing all validation failures.
     String token = config.getToken();
     String host = config.getHost();
