a009093be1586055846192d7613f7854a33b232a
Tanmay Rustagi <tanmay.rustagi@databricks.com>
2023-04-24 00:47:40 +0200

    --win testing

--- databricks-sdk-java/src/main/java/com/databricks/sdk/client/CliTokenSource.java
+++ databricks-sdk-java/src/main/java/com/databricks/sdk/client/CliTokenSource.java
@@ -79,9 +79,9 @@ public class CliTokenSource extends RefreshableTokenSource {
     if (System.getProperty("os.name").toLowerCase().startsWith("win")) {
       cmdTest = Arrays.asList("cmd.exe", "/c", "SET");
     } else if (System.getProperty("os.name").toLowerCase().startsWith("mac")) {
-      cmdTest = Arrays.asList("/bin/bash", "-c", "env");
+      cmdTest = Arrays.asList("/bin/bash", "-c", "cat", "/Users/runner/work/databricks-sdk-jvm/databricks-sdk-jvm/databricks-sdk-java/target/test-classes/testdata/az");
     } else {
-      cmdTest = Arrays.asList("/bin/bash", "-c", "cat", "/home/runner/work/databricks-sdk-jvm/databricks-sdk-jvm/databricks-sdk-java/target/test-classes/testdata/az");
+      cmdTest = Arrays.asList("/bin/bash", "-c", "cat /home/runner/work/databricks-sdk-jvm/databricks-sdk-jvm/databricks-sdk-java/target/test-classes/testdata/az");
     }
     ProcessBuilder processBuilder = new ProcessBuilder(cmdTest);
     LOG.info("tanmay -- map values -- " + getAllEnv.get().toString());
