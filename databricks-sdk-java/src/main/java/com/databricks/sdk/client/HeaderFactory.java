b5592edcf33a61de4a808d106900864e4875bd85
Tanmay Rustagi <tanmay.rustagi@databricks.com>
2023-04-25 20:52:02 +0200

    cleanup

--- databricks-sdk-java/src/main/java/com/databricks/sdk/client/HeaderFactory.java
+++ databricks-sdk-java/src/main/java/com/databricks/sdk/client/HeaderFactory.java
@@ -3,6 +3,8 @@ package com.databricks.sdk.client;
 import java.util.Map;
 
 public interface HeaderFactory {
-  /** @return a map of HTTP headers with refreshed tokens */
+  /**
+   * @return a map of HTTP headers with refreshed tokens
+   */
   Map<String, String> headers();
 }
