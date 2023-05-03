963b829f18748820516ca389b377ab373478064f
Serge Smertin <serge.smertin@databricks.com>
2023-05-03 18:48:20 +0200

    fix

--- databricks-sdk-java/src/main/java/com/databricks/sdk/client/HeaderFactory.java
+++ databricks-sdk-java/src/main/java/com/databricks/sdk/client/HeaderFactory.java
@@ -3,8 +3,6 @@ package com.databricks.sdk.client;
 import java.util.Map;
 
 public interface HeaderFactory {
-  /**
-   * @return a map of HTTP headers with refreshed tokens
-   */
+  /**·@return·a·map·of·HTTP·headers·with·refreshed·tokens·*/
   Map<String, String> headers();
 }
