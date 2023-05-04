48ca7cea199c121772c41da2e1ed3e23ddbd780c
Tanmay Rustagi <tanmay.rustagi@databricks.com>
2023-05-04 18:30:25 +0200

    add example

--- examples/cli-auth/src/main/java/com/databricks/sdk/CliAuthAccount.java
+++ examples/cli-auth/src/main/java/com/databricks/sdk/CliAuthAccount.java
@@ -1,15 +1,14 @@
 package com.databricks.sdk;
 
 import com.databricks.sdk.client.DatabricksConfig;
-import com.databricks.sdk.service.compute.ClusterInfo;
-import com.databricks.sdk.service.compute.ListClustersRequest;
+import com.databricks.sdk.service.provisioning.Workspace;
 
 /**
- Example for authenticating with Databricks through CLI.
+ Example for authenticating with Databricks Account through CLI.
  The authentication type can be set to either "bricks-cli" or "azure-cli".
  For details on authenticating via bricks cli, please see: <a href="https://docs.databricks.com/dev-tools/cli/auth-commands.html">...</a>
  */
-public class CliAuth {
+public class CliAuthAccount {
     /**
      Get config used for authenticating with Databricks.
      @return DatabricksConfig object used for authentication
@@ -17,17 +16,23 @@ public class CliAuth {
     private static DatabricksConfig getConfig() {
         // Change to "azure-cli" if you want to authenticate through azure cli
         String authType = "bricks-cli";
-        return new DatabricksConfig().setAuthType(authType);
+        String host = "";
+        String accountId = "";
+        return new DatabricksConfig()
+                .setAuthType(authType)
+                .setHost(host)
+                .setAccountId(accountId);
     }
 
     /**
-     Authenticate and retrieve the list of clusters from the workspace.
+     Authenticate and retrieve the list of clusters from the workspace
      */
     public static void main(String[] args) {
         DatabricksConfig config = getConfig();
-        DatabricksWorkspace workspace = new DatabricksWorkspace(config);
-        for (ClusterInfo c : workspace.clusters().list(new ListClustersRequest())) {
-            System.out.println(c.getClusterName());
+
+        DatabricksAccount account = new DatabricksAccount(config);
+        for (Workspace w : account.workspaces().list()) {
+            System.out.println(w.getWorkspaceName());
         }
     }
 }
---
+++ examples/cli-auth/src/main/java/com/databricks/sdk/CliAuthWorkspace.java
@@ -0,0 +1,34 @@
+package com.databricks.sdk;
+
+import com.databricks.sdk.client.DatabricksConfig;
+import com.databricks.sdk.service.compute.ClusterInfo;
+import com.databricks.sdk.service.compute.ListClustersRequest;
+
+/**
+ Example for authenticating with Databricks through CLI.
+ The authentication type can be set to either "bricks-cli" or "azure-cli".
+ For details on authenticating via bricks cli, please see: <a href="https://docs.databricks.com/dev-tools/cli/auth-commands.html">...</a>
+ */
+public class CliAuthWorkspace {
+    /**
+     Get config used for authenticating with Databricks.
+     @return DatabricksConfig object used for authentication
+     */
+    private static DatabricksConfig getConfig() {
+        // Change to "azure-cli" if you want to authenticate through azure cli
+        String authType = "bricks-cli";
+        return new DatabricksConfig().setAuthType(authType);
+    }
+
+    /**
+     Authenticate and retrieve the list of clusters from the workspace
+     */
+    public static void main(String[] args) {
+        DatabricksConfig config = getConfig();
+
+        DatabricksWorkspace workspace = new DatabricksWorkspace(config);
+        for (ClusterInfo c : workspace.clusters().list(new ListClustersRequest())) {
+            System.out.println(c.getClusterName());
+        }
+    }
+}
