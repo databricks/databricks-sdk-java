f591687b83fa2bd33130c167394eb864959a223a
Serge Smertin <259697+nfx@users.noreply.github.com>
2023-05-04 15:30:36 +0200

    Fix `az` perms in testing (#56)
    Directly set permissions on `az` script.

--- databricks-sdk-java/src/test/java/com/databricks/sdk/client/utils/TestOSUtils.java
+++ databricks-sdk-java/src/test/java/com/databricks/sdk/client/utils/TestOSUtils.java
@@ -1,8 +1,6 @@
 package com.databricks.sdk.client.utils;
 
 import java.io.File;
-import java.util.Arrays;
-import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -23,29 +21,5 @@ public interface TestOSUtils extends OSUtils {
   default String getTestDir() {
     String testDir = "/target/test-classes/";
     return testDir.replace("/", File.separator);
-  }
-
-  /**
-   * Returns a list of strings representing a command to set the az executable for testing purposes
-   * on Linux-based operating systems.
-   *
-   * @return a List of strings representing the executable command for the current operating system.
-   *     On Linux-based operating systems, the command will set execute permission for the az
-   *     executable in the test data directory. On other operating systems, it will return an empty
-   *     list.
-   */
-  default List<String> commandToSetTestAzExecutable() {
-    // Ubuntu:
-    // OLDPWD=/home/runner/work/databricks-sdk-jvm/databricks-sdk-jvm/databricks-sdk-java/target/test-classes/testdata
-    String cmd = "";
-    if (getOS().equals("linux")) {
-      cmd =
-          "chmod a+x "
-              + System.getenv("OLDPWD")
-              + "/target/test-classes/testdata/".replace("/", File.separator)
-              + "az";
-    }
-    LOG.info("Running command: {}", cmd);
-    return Arrays.asList("/bin/bash", "-c", cmd);
   }
 }
