31985c6028f8181f1b6a0b91a0ab09dd56e92e38
Tanmay <88379306+tanmay-db@users.noreply.github.com>
2023-05-05 10:16:31 +0200

    Make CLI list mutable and add example for account (#66)
    ## Changes
    <!-- Summary of your changes that are easy to understand -->
    Make list mutable - was missed because tests were run on workspace and
    we require addition only on account level. For azure this isn't required
    but good to do in case we need to update this in future.
    
    ## Tests
    <!-- How is this tested? -->
    Added class for test account - No auth issue, the following issue will
    be fixed in another PR.
    <img width="1327" alt="image"
    src="https://user-images.githubusercontent.com/88379306/236266262-64ee120e-4f94-4200-83ed-80a6aa8bd627.png">

---
+++ examples/cli-auth/src/main/java/com/databricks/sdk/CliAuthAccount.java
@@ -0,0 +1,41 @@
+package com.databricks.sdk;
+
+import com.databricks.sdk.client.DatabricksConfig;
+import com.databricks.sdk.service.provisioning.Workspace;
+
+/**
+ Example for authenticating with Databricks Account through CLI.
+ The authentication type can be set to either "bricks-cli" or "azure-cli".
+ For details on authenticating via bricks cli, please see: <a href="https://docs.databricks.com/dev-tools/cli/auth-commands.html">...</a>
+ */
+public class CliAuthAccount {
+    /**
+     Get config used for authenticating with Databricks.
+     @return DatabricksConfig object used for authentication
+     */
+    private static DatabricksConfig getConfig() {
+        // Change to "azure-cli" if you want to authenticate through azure cli
+        // Please authenticate using cli before using them in SDK.
+        // Example for the following case:
+        // $ bricks cli auth login --host <host> --account-id <accountId>
+        String authType = "bricks-cli";
+        String host = "";
+        String accountId = "";
+        return new DatabricksConfig()
+                .setAuthType(authType)
+                .setHost(host)
+                .setAccountId(accountId);
+    }
+
+    /**
+     Authenticate and retrieve the list of workspaces from account
+     */
+    public static void main(String[] args) {
+        DatabricksConfig config = getConfig();
+
+        DatabricksAccount account = new DatabricksAccount(config);
+        for (Workspace w : account.workspaces().list()) {
+            System.out.println(w.getWorkspaceName());
+        }
+    }
+}
--- examples/cli-auth/src/main/java/com/databricks/sdk/CliAuthWorkspace.java
+++ examples/cli-auth/src/main/java/com/databricks/sdk/CliAuthWorkspace.java
@@ -5,26 +5,33 @@ import com.databricks.sdk.service.compute.ClusterInfo;
 import com.databricks.sdk.service.compute.ListClustersRequest;
 
 /**
- Example for authenticating with Databricks through CLI.
+ Example for authenticating with Databricks Workspace through CLI.
  The authentication type can be set to either "bricks-cli" or "azure-cli".
  For details on authenticating via bricks cli, please see: <a href="https://docs.databricks.com/dev-tools/cli/auth-commands.html">...</a>
  */
-public class CliAuth {
+public class CliAuthWorkspace {
     /**
      Get config used for authenticating with Databricks.
      @return DatabricksConfig object used for authentication
      */
     private static DatabricksConfig getConfig() {
         // Change to "azure-cli" if you want to authenticate through azure cli
+        // Please authenticate using cli before using them in SDK.
+        // Example:
+        // $ bricks cli auth login --host <host>
         String authType = "bricks-cli";
-        return new DatabricksConfig().setAuthType(authType);
+        String host = "";
+        return new DatabricksConfig()
+                .setAuthType(authType)
+                .setHost(host);
     }
 
     /**
-     Authenticate and retrieve the list of clusters from the workspace.
+     Authenticate and retrieve the list of clusters from the workspace
      */
     public static void main(String[] args) {
         DatabricksConfig config = getConfig();
+
         DatabricksWorkspace workspace = new DatabricksWorkspace(config);
         for (ClusterInfo c : workspace.clusters().list(new ListClustersRequest())) {
             System.out.println(c.getClusterName());
