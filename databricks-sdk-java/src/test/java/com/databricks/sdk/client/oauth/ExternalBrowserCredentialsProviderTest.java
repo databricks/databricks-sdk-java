c5530d4722b191a71e7e7ceacb4feb4053d7d79f
Tanmay Rustagi <tanmay.rustagi@databricks.com>
2023-05-10 04:06:05 +0200

    61% coverage

--- databricks-sdk-java/src/test/java/com/databricks/sdk/client/oauth/ExternalBrowserCredentialsProviderTest.java
+++ databricks-sdk-java/src/test/java/com/databricks/sdk/client/oauth/ExternalBrowserCredentialsProviderTest.java
@@ -1,38 +1,39 @@
 package com.databricks.sdk.client.oauth;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import com.databricks.sdk.client.DatabricksConfig;
 import com.databricks.sdk.client.DatabricksException;
 import com.databricks.sdk.client.FixtureServer;
 import com.databricks.sdk.client.commons.CommonsHttpClient;
+import java.io.IOException;
 import org.junit.jupiter.api.Test;
 
-import java.io.IOException;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
 public class ExternalBrowserCredentialsProviderTest {
-    @Test
-    void OAuthClientTest() {
-        try (FixtureServer fixtures = new FixtureServer()) {
-            fixtures.with("GET /oidc/.well-known/oauth-authorization-server", "{\"\": []}");
-            String clientID = "test-client-id";
-            DatabricksConfig config = new DatabricksConfig()
-                    .setAuthType("external-browser")
-                    .setHost(fixtures.getUrl())
-                    .setClientId(clientID)
-                    .setHttpClient(new CommonsHttpClient(30));
-
-            config.resolve();
-            OAuthClient testClient;
-            try {
-                testClient = new OAuthClient(config);
-            } catch (IOException e) {
-                throw new DatabricksException(e.getMessage());
-            }
-            assertEquals(testClient.getClientId(), clientID);
-        } catch (IOException e) {
-            throw new DatabricksException(e.getMessage());
-        }
+  @Test
+  void OAuthClientTest() {
+    try (FixtureServer fixtures = new FixtureServer()) {
+      fixtures.with(
+          "GET /oidc/.well-known/oauth-authorization-server",
+          "{\"token_endpoint\": \"token-test-end-point\"}");
+      String clientID = "test-client-id";
+      DatabricksConfig config =
+          new DatabricksConfig()
+              .setAuthType("external-browser")
+              .setHost(fixtures.getUrl())
+              .setClientId(clientID)
+              .setHttpClient(new CommonsHttpClient(30));
+      config.resolve();
+      OAuthClient testClient;
+      try {
+        testClient = new OAuthClient(config);
+        Consent testConsent = testClient.initiateConsent();
+      } catch (IOException e) {
+        throw new DatabricksException(e.getMessage());
+      }
+      assertEquals(testClient.getClientId(), clientID);
+    } catch (IOException e) {
+      throw new DatabricksException(e.getMessage());
     }
+  }
 }
