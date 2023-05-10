b321a92ef1b21e0f5adcce7e0bebc0e017787d50
Tanmay Rustagi <tanmay.rustagi@databricks.com>
2023-05-10 13:35:43 +0200

    upd to core

--- databricks-sdk-java/src/test/java/com/databricks/sdk/client/oauth/ExternalBrowserCredentialsProviderTest.java
+++ databricks-sdk-java/src/test/java/com/databricks/sdk/client/oauth/ExternalBrowserCredentialsProviderTest.java
@@ -37,31 +37,4 @@ public class ExternalBrowserCredentialsProviderTest {
       throw new DatabricksException(e.getMessage());
     }
   }
-
-  @Test
-  void consentExchange() {
-    try (FixtureServer fixtures = new FixtureServer()) {
-      fixtures.with(
-              "GET /oidc/.well-known/oauth-authorization-server",
-              "{\"token_endpoint\": \"token-test-end-point\"}");
-      String clientID = "test-client-id";
-      DatabricksConfig config =
-              new DatabricksConfig()
-                      .setAuthType("external-browser")
-                      .setHost(fixtures.getUrl())
-                      .setClientId(clientID)
-                      .setHttpClient(new CommonsHttpClient(30));
-      config.resolve();
-
-      OAuthClient testClient = new OAuthClient(config);
-      assertEquals(testClient.getClientId(), clientID);
-
-      Consent testConsent = testClient.initiateConsent();
-      assertEquals(testConsent.getTokenUrl(), "token-test-end-point");
-
-      
-    } catch (IOException e) {
-      throw new DatabricksException(e.getMessage());
-    }
-  }
 }
