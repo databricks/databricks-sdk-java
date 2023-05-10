222982e8d4da9f6fb75d89ea1c59d42d2ee25ae0
Tanmay Rustagi <tanmay.rustagi@databricks.com>
2023-05-10 13:35:10 +0200

    upd to core

--- databricks-sdk-java/src/test/java/com/databricks/sdk/client/oauth/ExternalBrowserCredentialsProviderTest.java
+++ databricks-sdk-java/src/test/java/com/databricks/sdk/client/oauth/ExternalBrowserCredentialsProviderTest.java
@@ -2,11 +2,14 @@ package com.databricks.sdk.client.oauth;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import com.databricks.sdk.client.DatabricksConfig;
-import com.databricks.sdk.client.DatabricksException;
-import com.databricks.sdk.client.FixtureServer;
-import com.databricks.sdk.client.commons.CommonsHttpClient;
+import com.databricks.sdk.core.DatabricksConfig;
+import com.databricks.sdk.core.DatabricksException;
+import com.databricks.sdk.core.FixtureServer;
+import com.databricks.sdk.core.commons.CommonsHttpClient;
 import java.io.IOException;
+
+import com.databricks.sdk.core.oauth.Consent;
+import com.databricks.sdk.core.oauth.OAuthClient;
 import org.junit.jupiter.api.Test;
 
 public class ExternalBrowserCredentialsProviderTest {
@@ -30,6 +33,33 @@ public class ExternalBrowserCredentialsProviderTest {
 
       Consent testConsent = testClient.initiateConsent();
       assertEquals(testConsent.getTokenUrl(), "token-test-end-point");
+    } catch (IOException e) {
+      throw new DatabricksException(e.getMessage());
+    }
+  }
+
+  @Test
+  void consentExchange() {
+    try (FixtureServer fixtures = new FixtureServer()) {
+      fixtures.with(
+              "GET /oidc/.well-known/oauth-authorization-server",
+              "{\"token_endpoint\": \"token-test-end-point\"}");
+      String clientID = "test-client-id";
+      DatabricksConfig config =
+              new DatabricksConfig()
+                      .setAuthType("external-browser")
+                      .setHost(fixtures.getUrl())
+                      .setClientId(clientID)
+                      .setHttpClient(new CommonsHttpClient(30));
+      config.resolve();
+
+      OAuthClient testClient = new OAuthClient(config);
+      assertEquals(testClient.getClientId(), clientID);
+
+      Consent testConsent = testClient.initiateConsent();
+      assertEquals(testConsent.getTokenUrl(), "token-test-end-point");
+
+      
     } catch (IOException e) {
       throw new DatabricksException(e.getMessage());
     }
