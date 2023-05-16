d2eb944cd27902157fa0fb051a70d169a7e9f515
Tanmay Rustagi <tanmay.rustagi@databricks.com>
2023-05-16 13:16:40 +0200

    comments

--- databricks-sdk-java/src/test/java/com/databricks/sdk/core/oauth/ExternalBrowserCredentialsProviderTest.java
+++ databricks-sdk-java/src/test/java/com/databricks/sdk/core/oauth/ExternalBrowserCredentialsProviderTest.java
@@ -7,6 +7,7 @@ import com.databricks.sdk.core.DatabricksConfig;
 import com.databricks.sdk.core.DatabricksException;
 import com.databricks.sdk.core.FixtureServer;
 import com.databricks.sdk.core.commons.CommonsHttpClient;
+import com.databricks.sdk.core.http.HttpClient;
 import com.databricks.sdk.core.http.Request;
 import com.databricks.sdk.core.http.Response;
 import java.io.IOException;
@@ -18,7 +19,7 @@ import org.mockito.Mockito;
 
 public class ExternalBrowserCredentialsProviderTest {
   @Test
-  void clientAndConsentTest() {
+  void clientAndConsentTest() throws IOException {
     try (FixtureServer fixtures = new FixtureServer()) {
       fixtures.with(
           "GET /oidc/.well-known/oauth-authorization-server",
@@ -43,133 +44,114 @@ public class ExternalBrowserCredentialsProviderTest {
       assertTrue(testConsent.getAuthUrl().contains("response_type=code"));
       assertTrue(testConsent.getAuthUrl().contains("client_id=test-client-id"));
       assertTrue(testConsent.getAuthUrl().contains("redirect_uri=http://localhost:8080/callback"));
-    } catch (IOException e) {
-      throw new DatabricksException(e.getMessage());
     }
   }
 
   @Test
-  void openIDConnectEndPointsTestAccounts() {
-    try {
-      String testHost = "https://localhost:8080";
-      DatabricksConfig config =
-          new DatabricksConfig()
-              .setAuthType("external-browser")
-              .setHost("https://localhost:8080")
-              .setHttpClient(new CommonsHttpClient(30))
-              .setAccountId("testAccountId");
-      config.resolve();
+  void openIDConnectEndPointsTestAccounts() throws IOException {
+    String testHost = "https://localhost:8080";
+    DatabricksConfig config =
+        new DatabricksConfig()
+            .setAuthType("external-browser")
+            .setHost("https://localhost:8080")
+            .setHttpClient(new CommonsHttpClient(30))
+            .setAccountId("testAccountId");
+    config.resolve();
 
-      String prefix = testHost + "/oidc/accounts/" + config.getAccountId();
-      assertEquals(prefix + "/v1/token", config.getOidcEndpoints().getTokenEndpoint());
-      assertEquals(prefix + "/v1/authorize", config.getOidcEndpoints().getAuthorizationEndpoint());
-    } catch (IOException e) {
-      throw new DatabricksException(e.getMessage());
-    }
+    String prefix = testHost + "/oidc/accounts/" + config.getAccountId();
+    assertEquals(prefix + "/v1/token", config.getOidcEndpoints().getTokenEndpoint());
+    assertEquals(prefix + "/v1/authorize", config.getOidcEndpoints().getAuthorizationEndpoint());
   }
 
   @Test
-  void exchangeTest() {
+  void exchangeTest() throws IOException {
+    HttpClient hc = Mockito.mock(HttpClient.class);
+    String response =
+        "{\"access_token\": \"accessTokenFromServer\", \"token_type\": \"tokenTypeFromServer\", \"expires_in\": \"10\", \"refresh_token\": \"refreshTokenFromServer\"}";
+
+    // Mock because it's a POST Request to http client
+    Mockito.doReturn(new Response(response)).when(hc).execute(any(Request.class));
+
+    Consent testConsent =
+        new Consent.Builder()
+            .withClientId("testId")
+            .withClientSecret("testSecret")
+            .withAuthUrl("https://testUrl")
+            .withTokenUrl("testToken")
+            .withRedirectUrl("https://localhost:8080")
+            .withState("testState")
+            .withVerifier("testVerifier")
+            .withHttpClient(hc)
+            .build();
+
+    Map<String, String> queryError = new HashMap<>();
+    queryError.put("error", "testError");
+    queryError.put("error_description", "testErrorDescription");
     try {
-      CommonsHttpClient hc = Mockito.spy(new CommonsHttpClient(30));
-      String resp =
-          "{\"access_token\": \"accessTokenFromServer\", \"token_type\": \"tokenTypeFromServer\", \"expires_in\": \"10\", \"refresh_token\": \"refreshTokenFromServer\"}";
-
-      // Mock because it's a POST Request to http client
-      Mockito.doReturn(new Response(resp)).when(hc).execute(any(Request.class));
-
-      Consent mockConsent =
-          Mockito.spy(
-              new Consent.Builder()
-                  .withClientId("testId")
-                  .withClientSecret("testSecret")
-                  .withAuthUrl("https://testUrl")
-                  .withTokenUrl("testToken")
-                  .withRedirectUrl("https://localhost:8080")
-                  .withState("testState")
-                  .withVerifier("testVerifier")
-                  .withHttpClient(hc)
-                  .build());
-
-      Map<String, String> queryError = new HashMap<>();
-      queryError.put("error", "testError");
-      queryError.put("error_description", "testErrorDescription");
-      try {
-        mockConsent.exchangeCallbackParameters(queryError);
-      } catch (DatabricksException e) {
-        assertEquals("testError: testErrorDescription", e.getMessage());
-      }
-
-      Map<String, String> queryNoCodeCallback = new HashMap<>();
-      try {
-        mockConsent.exchangeCallbackParameters(queryNoCodeCallback);
-      } catch (DatabricksException e) {
-        assertEquals("No code returned in callback", e.getMessage());
-      }
-
-      Map<String, String> queryCreds = new HashMap<>();
-      queryCreds.put("code", "testCode");
-      queryCreds.put("state", "testState");
-      RefreshableCredentials creds = mockConsent.exchangeCallbackParameters(queryCreds);
-      assertEquals("accessTokenFromServer", creds.token.getAccessToken());
-      assertEquals("refreshTokenFromServer", creds.token.getRefreshToken());
-    } catch (IOException e) {
-      throw new DatabricksException(e.getMessage());
+      testConsent.exchangeCallbackParameters(queryError);
+    } catch (DatabricksException e) {
+      assertEquals("testError: testErrorDescription", e.getMessage());
     }
+
+    Map<String, String> queryNoCodeCallback = new HashMap<>();
+    try {
+      testConsent.exchangeCallbackParameters(queryNoCodeCallback);
+    } catch (DatabricksException e) {
+      assertEquals("No code returned in callback", e.getMessage());
+    }
+
+    Map<String, String> queryCreds = new HashMap<>();
+    queryCreds.put("code", "testCode");
+    queryCreds.put("state", "testState");
+    RefreshableCredentials creds = testConsent.exchangeCallbackParameters(queryCreds);
+    assertEquals("accessTokenFromServer", creds.token.getAccessToken());
+    assertEquals("refreshTokenFromServer", creds.token.getRefreshToken());
   }
 
   @Test
-  void clientCredentials() {
-    try {
-      CommonsHttpClient hc = Mockito.spy(new CommonsHttpClient(30));
-      String resp =
-          "{\"access_token\": \"accessTokenFromServer\", \"token_type\": \"tokenTypeFromServer\", \"expires_in\": \"10\", \"refresh_token\": \"refreshTokenFromServer\"}";
+  void clientCredentials() throws IOException {
+    HttpClient hc = Mockito.mock(HttpClient.class);
+    String response =
+        "{\"access_token\": \"accessTokenFromServer\", \"token_type\": \"tokenTypeFromServer\", \"expires_in\": \"10\", \"refresh_token\": \"refreshTokenFromServer\"}";
+    Mockito.doReturn(new Response(response)).when(hc).execute(any(Request.class));
 
-      Mockito.doReturn(new Response(resp)).when(hc).execute(any(Request.class));
-      ClientCredentials clientCredentials =
-          new ClientCredentials.Builder()
-              .withHttpClient(hc)
-              .withClientId("testClientId")
-              .withClientSecret("abc")
-              .withTokenUrl("https://tokenUrl")
-              .build();
-      Token token = clientCredentials.refresh();
-      assertEquals("accessTokenFromServer", token.getAccessToken());
-      assertEquals("refreshTokenFromServer", token.getRefreshToken());
-    } catch (IOException e) {
-      throw new DatabricksException(e.getMessage());
-    }
+    ClientCredentials clientCredentials =
+        new ClientCredentials.Builder()
+            .withHttpClient(hc)
+            .withClientId("testClientId")
+            .withClientSecret("abc")
+            .withTokenUrl("https://tokenUrl")
+            .build();
+    Token token = clientCredentials.refresh();
+    assertEquals("accessTokenFromServer", token.getAccessToken());
+    assertEquals("refreshTokenFromServer", token.getRefreshToken());
   }
 
   @Test
-  void refreshableCredentials() {
-    try {
-      CommonsHttpClient hc = Mockito.spy(new CommonsHttpClient(30));
-      String resp =
-          "{\"access_token\": \"accessTokenFromServer\", \"token_type\": \"tokenTypeFromServer\", \"expires_in\": \"10\", \"refresh_token\": \"refreshTokenFromServer\"}";
+  void refreshableCredentials() throws IOException {
+    HttpClient hc = Mockito.mock(HttpClient.class);
+    String response =
+        "{\"access_token\": \"accessTokenFromServer\", \"token_type\": \"tokenTypeFromServer\", \"expires_in\": \"10\", \"refresh_token\": \"refreshTokenFromServer\"}";
+    Mockito.doReturn(new Response(response)).when(hc).execute(any(Request.class));
 
-      Mockito.doReturn(new Response(resp)).when(hc).execute(any(Request.class));
-      RefreshableCredentials refreshableCredentials =
-          new RefreshableCredentials.Builder()
-              .withHttpClient(hc)
-              .withClientId("testClientId")
-              .withClientSecret("abc")
-              .withTokenUrl("https://tokenUrl")
-              .withToken(
-                  new Token(
-                      "originalAccessToken",
-                      "originalTokenType",
-                      "originalRefreshToken",
-                      LocalDateTime.MAX))
-              .build();
-      Token token = refreshableCredentials.refresh();
+    RefreshableCredentials refreshableCredentials =
+        new RefreshableCredentials.Builder()
+            .withHttpClient(hc)
+            .withClientId("testClientId")
+            .withClientSecret("abc")
+            .withTokenUrl("https://tokenUrl")
+            .withToken(
+                new Token(
+                    "originalAccessToken",
+                    "originalTokenType",
+                    "originalRefreshToken",
+                    LocalDateTime.MAX))
+            .build();
+    Token token = refreshableCredentials.refresh();
 
-      // We check that we are actually getting the token from server response (that is defined
-      // above) rather than what was given while creating refreshable credentials
-      assertEquals("accessTokenFromServer", token.getAccessToken());
-      assertEquals("refreshTokenFromServer", token.getRefreshToken());
-    } catch (IOException e) {
-      throw new DatabricksException(e.getMessage());
-    }
+    // We check that we are actually getting the token from server response (that is defined
+    // above) rather than what was given while creating refreshable credentials
+    assertEquals("accessTokenFromServer", token.getAccessToken());
+    assertEquals("refreshTokenFromServer", token.getRefreshToken());
   }
 }
--- databricks-sdk-java/src/test/java/com/databricks/sdk/mixin/DbfsExtTest.java
+++ databricks-sdk-java/src/test/java/com/databricks/sdk/mixin/DbfsExtTest.java
@@ -4,7 +4,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 
 import com.databricks.sdk.core.ApiClient;
-import com.databricks.sdk.core.DatabricksException;
 import com.databricks.sdk.service.files.*;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
@@ -15,31 +14,24 @@ import org.mockito.Mockito;
 
 public class DbfsExtTest {
   @Test
-  void write() {
-    try {
-      String filePath = "/a/b/c";
-      String data =
-          Base64.getEncoder().encodeToString("test data".getBytes(StandardCharsets.UTF_8));
+  void write() throws IOException {
+    String filePath = "/a/b/c";
+    String data = Base64.getEncoder().encodeToString("test data".getBytes(StandardCharsets.UTF_8));
 
-      ApiClient apiClient = new ApiClient();
-      DbfsExt mockedDbfsExt = Mockito.spy(new DbfsExt(apiClient));
+    ApiClient apiClient = new ApiClient();
+    DbfsExt mockedDbfsExt = Mockito.spy(new DbfsExt(apiClient));
 
-      // Mock external create, addBlock and close requests
-      Mockito.doReturn(new CreateResponse().setHandle(0L))
-          .when(mockedDbfsExt)
-          .create(any(Create.class));
-      Mockito.doNothing().when(mockedDbfsExt).addBlock(any(AddBlock.class));
-      Mockito.doNothing().when(mockedDbfsExt).close(any(Close.class));
+    // Mock external create, addBlock and close requests
+    Mockito.doReturn(new CreateResponse().setHandle(0L))
+        .when(mockedDbfsExt)
+        .create(any(Create.class));
+    Mockito.doNothing().when(mockedDbfsExt).addBlock(any(AddBlock.class));
+    Mockito.doNothing().when(mockedDbfsExt).close(any(Close.class));
 
-      String testPath =
-          mockedDbfsExt
-              .write(Paths.get(filePath), data.getBytes(StandardCharsets.UTF_8))
-              .toString();
+    String testPath =
+        mockedDbfsExt.write(Paths.get(filePath), data.getBytes(StandardCharsets.UTF_8)).toString();
 
-      // Returned path should match the original
-      assertEquals(filePath, testPath);
-    } catch (IOException e) {
-      throw new DatabricksException(e.getMessage());
-    }
+    // Returned path should match the original
+    assertEquals(filePath, testPath);
   }
 }
