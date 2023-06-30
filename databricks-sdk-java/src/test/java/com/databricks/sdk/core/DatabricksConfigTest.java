b8dca42481713f22cb19d2b8fd4d84f6cf644769
Miles Yucht <miles@databricks.com>
2023-06-30 15:57:57 +0200

    spotless

--- databricks-sdk-java/src/test/java/com/databricks/sdk/core/DatabricksConfigTest.java
+++ databricks-sdk-java/src/test/java/com/databricks/sdk/core/DatabricksConfigTest.java
@@ -1,23 +1,28 @@
 package com.databricks.sdk.core;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
 
 public class DatabricksConfigTest {
   @Test
   public void testIsAccountHost() {
-    assertTrue(new DatabricksConfig().setHost("https://accounts.azuredatabricks.net").isAccountClient());
+    assertTrue(
+        new DatabricksConfig().setHost("https://accounts.azuredatabricks.net").isAccountClient());
   }
 
   @Test
   public void testIsAccountHostDod() {
-    assertTrue(new DatabricksConfig().setHost("https://accounts-dod.cloud.databricks.us").isAccountClient());
+    assertTrue(
+        new DatabricksConfig()
+            .setHost("https://accounts-dod.cloud.databricks.us")
+            .isAccountClient());
   }
 
   @Test
   public void testIsAccountHostWorkspace() {
-    assertFalse(new DatabricksConfig().setHost("https://westeurope.azuredatabricks.net").isAccountClient());
+    assertFalse(
+        new DatabricksConfig().setHost("https://westeurope.azuredatabricks.net").isAccountClient());
   }
 }
