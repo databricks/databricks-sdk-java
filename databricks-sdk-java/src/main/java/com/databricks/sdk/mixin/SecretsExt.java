f9392ce2c2ef3db7b6c872dc60c611f8eba33340
Tanmay Rustagi <tanmay.rustagi@databricks.com>
2023-08-14 12:23:32 +0530

    tests

--- databricks-sdk-java/src/main/java/com/databricks/sdk/mixin/SecretsExt.java
+++ databricks-sdk-java/src/main/java/com/databricks/sdk/mixin/SecretsExt.java
@@ -1,25 +1,27 @@
 package com.databricks.sdk.mixin;
 
 import com.databricks.sdk.core.ApiClient;
-import com.databricks.sdk.service.workspace.GetSecretResponse;
-import com.databricks.sdk.service.workspace.SecretMetadata;
-import com.databricks.sdk.service.workspace.SecretScope;
-import com.databricks.sdk.service.workspace.SecretsAPI;
+import com.databricks.sdk.service.files.DbfsService;
+import com.databricks.sdk.service.workspace.*;
+
 import java.util.ArrayList;
 import java.util.Base64;
 import java.util.List;
 
 /** Remote equivalent of secrets util. */
-public class SecretsExt {
-  private final SecretsAPI api;
+public class SecretsExt extends SecretsAPI{
 
-  public SecretsExt() {
-    this.api = new SecretsAPI(new ApiClient());
+  public SecretsExt(ApiClient apiClient) {
+    super(apiClient);
+  }
+
+  public SecretsExt(SecretsService mock) {
+    super(mock);
   }
 
   /** Gets the bytes representation of a secret value for the specified scope and key. */
   public byte[] getBytes(String scope, String key) {
-    GetSecretResponse response = this.api.getSecret(scope, key);
+    GetSecretResponse response = this.getSecret(scope, key);
     return Base64.getDecoder().decode(response.getValue());
   }
 
@@ -34,7 +36,7 @@ public class SecretsExt {
    * equivalent Java class.
    */
   public List<SecretMetadata> list(String scope) {
-    Iterable<SecretMetadata> secretMetadatas = this.api.listSecrets(scope);
+    Iterable<SecretMetadata> secretMetadatas = this.listSecrets(scope);
     List<SecretMetadata> secretMetadataList = new ArrayList<>();
     for (SecretMetadata s : secretMetadatas) {
       secretMetadataList.add(s);
@@ -44,7 +46,7 @@ public class SecretsExt {
 
   /** Lists the available scopes. Transform from SDK dataclass to equivalent Java class. */
   public List<SecretScope> listScopes() {
-    Iterable<SecretScope> secretScopes = this.api.listScopes();
+    Iterable<SecretScope> secretScopes = this.listScopes();
     List<SecretScope> secretScopeList = new ArrayList<>();
     for (SecretScope s : secretScopes) {
       secretScopeList.add(s);
--- databricks-sdk-java/src/test/java/com/databricks/sdk/mixin/SecretsExtTest.java
+++ databricks-sdk-java/src/test/java/com/databricks/sdk/mixin/SecretsExtTest.java
@@ -1,30 +1,53 @@
 package com.databricks.sdk.mixin;
 
-import com.databricks.sdk.core.FixtureServer;
-import com.databricks.sdk.service.workspace.SecretMetadata;
-import java.io.IOException;
-import java.util.List;
+import com.databricks.sdk.service.workspace.*;
 
-import com.databricks.sdk.service.workspace.SecretScope;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SecretsExtTest {
+  @Mock
+  SecretsService mockedSecretsService;
 
-  @Test
-  void listTest() throws IOException {
-    try (FixtureServer fixtures = new FixtureServer()) {
-      SecretsExt secretsExt = new SecretsExt();
-      List<SecretMetadata> metadataList = secretsExt.list("testScope");
-      assert (metadataList.isEmpty());
-    }
+  @BeforeEach
+  void setup() {
+    MockitoAnnotations.openMocks(this);
   }
 
   @Test
-  void listScopes() throws IOException {
-    try (FixtureServer fixtures = new FixtureServer()) {
-      SecretsExt secretsExt = new SecretsExt();
-      List<SecretScope> metadataList = secretsExt.listScopes();
-      assert (metadataList.isEmpty());
-    }
+  void getBytesTest() {
+    SecretsExt mockedSecretExt = new SecretsExt(mockedSecretsService);
+    String encodedValue = Base64.getEncoder()
+            .encodeToString("testValueBase64Decoded".getBytes(StandardCharsets.UTF_8));
+
+    Mockito.doReturn(new GetSecretResponse().setKey("testKey").setValue(encodedValue))
+            .when(mockedSecretsService)
+            .getSecret(new GetSecretRequest().setScope("abc").setKey("xyz"));
+
+    byte[] res = mockedSecretExt.getBytes("abc", "xyz");
+    assertEquals("testValueBase64Decoded", new String(res, StandardCharsets.UTF_8));
+  }
+
+  @Test
+  void getTest() {
+    SecretsExt mockedSecretExt = new SecretsExt(mockedSecretsService);
+    String encodedValue = Base64.getEncoder()
+            .encodeToString("testValueBase64Decoded".getBytes(StandardCharsets.UTF_8));
+
+    Mockito.doReturn(new GetSecretResponse().setKey("testKey").setValue(encodedValue))
+            .when(mockedSecretsService)
+            .getSecret(new GetSecretRequest().setScope("abc").setKey("xyz"));
+
+    String res = mockedSecretExt.get("abc", "xyz");
+    assertEquals("testValueBase64Decoded", res);
   }
 }
