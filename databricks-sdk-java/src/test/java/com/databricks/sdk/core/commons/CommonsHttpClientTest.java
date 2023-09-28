6b92a5a39aa9181199ea7395316bfa7801b2177e
Miles Yucht <miles@databricks.com>
2023-09-28 16:06:32 +0200

    Chunk streaming request bodies only (#157)
    ## Changes
    #138 changed the CommonsHttpClient implementation to use
    `InputStreamEntity` for all request bodies sent using the Java SDK. As a
    result, requests are sent with a `Transfer-Encoding: chunked` header,
    and the request body is chunked accordingly. However, the Databricks
    REST API only tolerates chunked requests for certain APIs; for others,
    it ignores the request body if Content-Length is not specified. Auth
    falls under that category, which caused #154.
    
    To fix this, Request will keep track of string entities and input stream
    entities separately. Previously, Request had separate fields for the
    body and debugBody, but they were both always set, so it wasn't possible
    to distinguish between these two cases. Now, HTTP clients can have
    different behavior based on whether the request body can be fully
    materialized as a string or is lazily read as with input streams.
    
    As part of this, I have removed `SimpleHttpServer`. This was used before
    we were sure whether it was possible to use the HttpServer built into
    the JDK. Now that we are confident that we can (see usage in
    ExternalBrowserCredentialsProvider.java), I've replaced it with
    HttpServer and a custom HttpHandler in FixtureServer (the only place
    where it is used now).
    
    One other small change: I've updated the logging configuration and
    dependency for the cli auth demo app. This ensures that users can see
    debug output (which I used when debugging this).
    
    Closes #154. 
    
    ## Tests
    I've refactored FixtureServer to support more advanced validation on the
    HTTP requests sent by clients for testing purposes. Now, users can
    assert that a request has a specific method, path, headers, body, and
    check for headers that should not be present.
    
    One downside of this change is that users need to call the `.with()`
    method once per API call, rather than one time, at the start of each
    test.
    
    Unit tests cover both cases (request body should be chunked when
    specified as a String; request body should not be chunked when specified
    as an InputStream).

--- databricks-sdk-java/src/test/java/com/databricks/sdk/core/commons/CommonsHttpClientTest.java
+++ databricks-sdk-java/src/test/java/com/databricks/sdk/core/commons/CommonsHttpClientTest.java
@@ -7,16 +7,61 @@ import com.databricks.sdk.core.http.HttpClient;
 import com.databricks.sdk.core.http.Request;
 import com.databricks.sdk.core.http.Response;
 import java.io.IOException;
+import java.util.*;
+import org.apache.commons.io.IOUtils;
 import org.junit.jupiter.api.Test;
 
 class CommonsHttpClientTest {
   @Test
   public void itWorks() throws IOException {
-    try (FixtureServer fixtures = new FixtureServer().with("GET /foo?x=y", "bar")) {
+    try (FixtureServer fixtures = new FixtureServer().with("GET", "/foo?x=y", "bar")) {
       HttpClient httpClient = new CommonsHttpClient(30);
       Request in = new Request("GET", fixtures.getUrl() + "/foo").withQueryParam("x", "y");
       Response out = httpClient.execute(in);
       assertEquals("bar", out.getDebugBody().trim());
+    }
+  }
+
+  // Test that requests with a string body are sent with no Transfer-Encoding header and with a
+  // Content-Length header.
+  @Test
+  public void testStringBody() throws IOException {
+    FixtureServer.FixtureMapping fixture =
+        new FixtureServer.FixtureMapping.Builder()
+            .validatePath("/foo")
+            .validateMethod("POST")
+            .validateHeadersPresent(
+                Collections.singletonMap("Content-Length", Collections.singletonList("3")))
+            .validateHeadersAbsent(Collections.singletonList("Transfer-Encoding"))
+            .withResponse("quux")
+            .build();
+    try (FixtureServer fixtures = new FixtureServer().with(fixture)) {
+      HttpClient httpClient = new CommonsHttpClient(30);
+      Request in = new Request("POST", fixtures.getUrl() + "/foo", "bar");
+      Response out = httpClient.execute(in);
+      assertEquals("quux", out.getDebugBody().trim());
+    }
+  }
+
+  // Test that requests with an InputStream body are sent with a Transfer-Encoding header and
+  // without a Content-Length header.
+  @Test
+  public void testInputStreamBody() throws IOException {
+    FixtureServer.FixtureMapping fixture =
+        new FixtureServer.FixtureMapping.Builder()
+            .validatePath("/foo")
+            .validateMethod("POST")
+            .validateHeadersPresent(
+                Collections.singletonMap("Transfer-Encoding", Collections.singletonList("chunked")))
+            .validateHeadersAbsent(Collections.singletonList("Content-Length"))
+            .withResponse("quux")
+            .build();
+    try (FixtureServer fixtures = new FixtureServer().with(fixture)) {
+      HttpClient httpClient = new CommonsHttpClient(30);
+      Request in =
+          new Request("POST", fixtures.getUrl() + "/foo", IOUtils.toInputStream("bar", "UTF-8"));
+      Response out = httpClient.execute(in);
+      assertEquals("quux", out.getDebugBody().trim());
     }
   }
 }
