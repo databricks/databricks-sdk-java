28f1139a8a0262665b98d53905d3a59afee0f00a
Miles Yucht <miles@databricks.com>
2023-11-24 10:08:51 +0100

    fix test

--- databricks-sdk-java/src/test/java/com/databricks/sdk/core/ConfigResolving.java
+++ databricks-sdk-java/src/test/java/com/databricks/sdk/core/ConfigResolving.java
@@ -1,13 +1,21 @@
 package com.databricks.sdk.core;
 
 import com.databricks.sdk.core.utils.Environment;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 import java.util.function.Supplier;
 
 public interface ConfigResolving {
 
   default void resolveConfig(DatabricksConfig config, Supplier<Map<String, String>> envGetter) {
-    String[] path = envGetter.get().get("PATH").split(":");
+    String pathStr = envGetter.get().get("PATH");
+    List<String> path = new ArrayList<>();
+    if (pathStr != null) {
+      path.addAll(Arrays.asList(pathStr.split(":")));
+    }
     Environment env = new Environment(envGetter.get(), path);
     config.resolve(env);
   }
