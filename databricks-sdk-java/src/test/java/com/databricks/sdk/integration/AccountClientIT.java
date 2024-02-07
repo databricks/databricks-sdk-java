413216e3947a567c175d9877427564cf29ffb589
Miles Yucht <miles@databricks.com>
2024-02-07 16:37:22 +0100

    fmt

--- databricks-sdk-java/src/test/java/com/databricks/sdk/integration/AccountClientIT.java
+++ databricks-sdk-java/src/test/java/com/databricks/sdk/integration/AccountClientIT.java
@@ -6,17 +6,15 @@ import com.databricks.sdk.integration.framework.EnvContext;
 import com.databricks.sdk.integration.framework.EnvOrSkip;
 import com.databricks.sdk.integration.framework.EnvTest;
 import com.databricks.sdk.service.provisioning.Workspace;
-import java.util.Iterator;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-
-import static java.lang.Long.parseLong;
 
 @EnvContext("account")
 @ExtendWith(EnvTest.class)
 public class AccountClientIT {
   @Test
-  public void getWorkspaceClient(AccountClient a, @EnvOrSkip("TEST_WORKSPACE_ID") String workspaceIdStr) {
+  public void getWorkspaceClient(
+      AccountClient a, @EnvOrSkip("TEST_WORKSPACE_ID") String workspaceIdStr) {
     long workspaceId = Long.parseLong(workspaceIdStr);
     Workspace workspace = a.workspaces().get(workspaceId);
     WorkspaceClient w = a.getWorkspaceClient(workspace);
