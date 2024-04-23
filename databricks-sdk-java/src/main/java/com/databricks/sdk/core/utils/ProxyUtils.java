ac1d5a283e8d5e7e91ecf53f1d399a56612c955c
Vikrant Puppala <vikrant.puppala@databricks.com>
2024-04-23 14:14:59 +0530

    [PECO-1542] Add a way to provide proxy details to SDK (#271)
    ## Changes
    <!-- Summary of your changes that are easy to understand -->
    Duplicate of #242. Re-created to sign commits. (This PR adds a way to
    provide proxy configs in the SDK directly, ability to pick up the system
    properties and authentication for proxy via basic or negotiate-kerberos
    schemes)
    
    ## Tests
    <!-- How is this tested? -->
    Documented in #242 (These changes were tested in an environment
    consisting of a proxy server with kerberos authentication hosted on
    Ubuntu and Windows VMs. The detailed testing details are documented
    here:
    https://docs.google.com/document/d/1Zxlfx-R_JytFqMZfQMBfSQ8mj4W-aHa60vw4KdjUkEc/edit)

---
+++ databricks-sdk-java/src/main/java/com/databricks/sdk/core/utils/ProxyUtils.java
@@ -0,0 +1,143 @@
+package com.databricks.sdk.core.utils;
+
+import com.databricks.sdk.core.DatabricksException;
+import com.databricks.sdk.core.ProxyConfig;
+import java.security.Principal;
+import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthSchemeProvider;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.Credentials;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.client.config.AuthSchemes;
+import org.apache.http.config.RegistryBuilder;
+import org.apache.http.impl.auth.SPNegoSchemeFactory;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.client.ProxyAuthenticationStrategy;
+
+/**
+ * This class is used to setup the proxy configs for the http client. This includes setting up the
+ * proxy host, port, and authentication.
+ */
+public class ProxyUtils {
+
+  /**
+   * Setup the proxy configuration in the http client builder.
+   *
+   * @param config the proxy configuration
+   * @param builder the http client builder
+   */
+  public static void setupProxy(ProxyConfig config, HttpClientBuilder builder) {
+    String proxyHost = null;
+    Integer proxyPort = null;
+    String proxyUser = null;
+    String proxyPassword = null;
+    if (config.getUseSystemProperties() != null && config.getUseSystemProperties()) {
+      builder.useSystemProperties();
+      String protocol = System.getProperty("https.proxyHost") != null ? "https" : "http";
+      proxyHost = System.getProperty(protocol + ".proxyHost");
+      proxyPort = Integer.parseInt(System.getProperty(protocol + ".proxyPort"));
+      proxyUser = System.getProperty(protocol + ".proxyUser");
+      proxyPassword = System.getProperty(protocol + ".proxyPassword");
+    }
+    // Override system properties if proxy configuration is explicitly set
+    if (config.getHost() != null) {
+      proxyHost = config.getHost();
+      proxyPort = config.getPort();
+      proxyUser = config.getUsername();
+      proxyPassword = config.getPassword();
+      builder.setProxy(new HttpHost(proxyHost, proxyPort));
+    }
+    setupProxyAuth(
+        proxyHost, proxyPort, config.getProxyAuthType(), proxyUser, proxyPassword, builder);
+  }
+
+  /**
+   * This method sets up the proxy authentication in the http client builder.
+   *
+   * @param proxyHost the proxy host
+   * @param proxyPort the proxy port
+   * @param proxyAuthType the proxy authentication type
+   * @param proxyUser the proxy user
+   * @param proxyPassword the proxy password
+   * @param builder the http client builder
+   */
+  public static void setupProxyAuth(
+      String proxyHost,
+      Integer proxyPort,
+      ProxyConfig.ProxyAuthType proxyAuthType,
+      String proxyUser,
+      String proxyPassword,
+      HttpClientBuilder builder) {
+    if (proxyAuthType == null) {
+      return;
+    }
+    AuthScope authScope = new AuthScope(proxyHost, proxyPort);
+    switch (proxyAuthType) {
+      case NONE:
+        break;
+      case BASIC:
+        setupBasicProxyAuth(builder, authScope, proxyUser, proxyPassword);
+        break;
+      case SPNEGO:
+        setupNegotiateProxyAuth(builder, authScope);
+        break;
+      default:
+        throw new DatabricksException("Unknown proxy auth type: " + proxyAuthType);
+    }
+  }
+
+  /**
+   * This method sets up the proxy authentication using the negotiate mechanism in the http client
+   * builder.
+   *
+   * @param builder the http client builder
+   * @param authScope the authentication scope
+   */
+  public static void setupNegotiateProxyAuth(HttpClientBuilder builder, AuthScope authScope) {
+    // We only support kerberos for negotiate as of now
+    System.setProperty("javax.security.auth.useSubjectCredsOnly", "false");
+    // "java.security.krb5.conf" system property needs to be set if krb5.conf is not in the default
+    // location
+    // Use "sun.security.krb5.debug" and "sun.security.jgss.debug" system properties for debugging
+    Credentials useJaasCreds =
+        new Credentials() {
+          public String getPassword() {
+            return null;
+          }
+
+          public Principal getUserPrincipal() {
+            return null;
+          }
+        };
+
+    CredentialsProvider credsProvider = new BasicCredentialsProvider();
+    credsProvider.setCredentials(authScope, useJaasCreds);
+    builder
+        .setDefaultCredentialsProvider(credsProvider)
+        .setDefaultAuthSchemeRegistry(
+            RegistryBuilder.<AuthSchemeProvider>create()
+                .register(AuthSchemes.SPNEGO, new SPNegoSchemeFactory(true))
+                .build());
+  }
+
+  /**
+   * This method sets up the proxy authentication using the basic mechanism credentials provided
+   * into the http client builder.
+   *
+   * @param builder the http client builder
+   * @param authScope the authentication scope
+   * @param proxyUser the proxy user
+   * @param proxyPassword the proxy password
+   */
+  public static void setupBasicProxyAuth(
+      HttpClientBuilder builder, AuthScope authScope, String proxyUser, String proxyPassword) {
+    CredentialsProvider credsProvider = new BasicCredentialsProvider();
+    credsProvider.setCredentials(
+        authScope, new UsernamePasswordCredentials(proxyUser, proxyPassword));
+    builder
+        .setDefaultCredentialsProvider(credsProvider)
+        .setProxyAuthenticationStrategy(new ProxyAuthenticationStrategy());
+  }
+}
