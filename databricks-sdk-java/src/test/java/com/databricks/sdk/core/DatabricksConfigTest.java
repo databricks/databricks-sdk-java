4d830b5bee6c996b1d885e542d037ea82e517ed8
Miles Yucht <miles@databricks.com>
2024-04-30 09:26:55 +0200

    Fix OIDC Endpoint Fetching in DatabricksConfig for Workspace Clients (#277)
    ## Changes
    The logic for determining OIDC endpoints depends on whether a user is
    authenticating to the workspace or account. The Java SDK uses presence
    of account ID to make this decision, which is actually incorrect: logic
    for determining whether a client is configured at the workspace or
    account-level is based on the hostname. This PR fixes this logic to
    match the Python SDK. As a result, users are able to set the account ID
    configuration for Workspace clients and will still be able to
    authenticate with OAuth.
    
    ## Tests
    - [x] Some unit tests added to verify this behavior.
    - [x] Manually verified the old behavior, and verified that this doesn't
    recur post-fix.

--- databricks-sdk-java/src/test/java/com/databricks/sdk/core/DatabricksConfigTest.java
+++ databricks-sdk-java/src/test/java/com/databricks/sdk/core/DatabricksConfigTest.java
@@ -1,8 +1,8 @@
 package com.databricks.sdk.core;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.IOException;
 import org.junit.jupiter.api.Test;
 
 public class DatabricksConfigTest {
@@ -48,5 +48,33 @@ public class DatabricksConfigTest {
   public void testIsAzureUsGov() {
     assertTrue(
         new DatabricksConfig().setHost("https://adb-1234567890.0.databricks.azure.us/").isAzure());
+  }
+
+  @Test
+  public void testWorkspaceLevelOidcEndpointsWithAccountId() throws IOException {
+    try (FixtureServer server =
+        new FixtureServer()
+            .with(
+                "GET",
+                "/oidc/.well-known/oauth-authorization-server",
+                "{\"authorization_endpoint\":\"https://test-workspace.cloud.databricks.com/oidc/v1/authorize\"}")) {
+      DatabricksConfig c =
+          new DatabricksConfig().setHost(server.getUrl()).setAccountId("1234567890");
+      c.resolve();
+      assertEquals(
+          c.getOidcEndpoints().getAuthorizationEndpoint(),
+          "https://test-workspace.cloud.databricks.com/oidc/v1/authorize");
+    }
+  }
+
+  @Test
+  public void testAccountLevelOidcEndpoints() throws IOException {
+    assertEquals(
+        new DatabricksConfig()
+            .setHost("https://accounts.cloud.databricks.com")
+            .setAccountId("1234567890")
+            .getOidcEndpoints()
+            .getAuthorizationEndpoint(),
+        "https://accounts.cloud.databricks.com/oidc/accounts/1234567890/v1/authorize");
   }
 }
