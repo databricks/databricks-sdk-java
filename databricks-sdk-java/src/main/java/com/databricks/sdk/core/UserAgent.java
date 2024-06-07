0122ba75b7ac58460894e394ca832977ab102eee
Miles Yucht <miles@databricks.com>
2024-06-07 17:09:59 +0200

    Support partners in SDK (#291)
    ## Changes
    This PR is a port of
    https://github.com/databricks/databricks-sdk-go/pull/925 to the Java
    SDK.
    
    Partners of Databricks need a mechanism to register themselves in
    libraries or applications that they write. In this way, requests made by
    users of those libraries will include sufficient information to link
    those requests to the original users.
    
    This PR adds a new function `withPartner` that can be used by a partner
    to add partner information to the User-Agent header for requests made by
    the SDK. The new header will have the form `partner/<parther id>`. The
    partner identifier is opaque for the SDK, but it must be alphanumeric.
    
    This PR also lifts the requirement that there may only be a single copy
    of an entry in the user agent. This allows multiple partners to register
    themselves in the same library or application.
    
    Additionally, this PR adds validation on keys and values of entries in
    the user agent to match the Go SDK: alphanumeric keys and alphanumeric
    or semver values.
    
    ## Tests
    Unit tests cover the implementation of the `UserAgent` class.

--- databricks-sdk-java/src/main/java/com/databricks/sdk/core/UserAgent.java
+++ databricks-sdk-java/src/main/java/com/databricks/sdk/core/UserAgent.java
@@ -1,14 +1,33 @@
 package com.databricks.sdk.core;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class UserAgent {
   private static String product = "unknown";
   private static String productVersion = "0.0.0";
 
-  private static final Map<String, String> otherInfo = new HashMap<>();
+  private static class Info {
+    private String key;
+    private String value;
+
+    public Info(String key, String value) {
+      this.key = key;
+      this.value = value;
+    }
+
+    public String getKey() {
+      return key;
+    }
+
+    public String getValue() {
+      return value;
+    }
+  }
+
+  private static final ArrayList<Info> otherInfo = new ArrayList<>();
 
   // TODO: check if reading from
   // /META-INF/maven/com.databricks/databrics-sdk-java/pom.properties
@@ -20,8 +39,60 @@ public class UserAgent {
     UserAgent.productVersion = productVersion;
   }
 
+  // Regular expression copied from https://semver.org/.
+  private static final String semVerCore = "(0|[1-9]\\d*)\\.(0|[1-9]\\d*)\\.(0|[1-9]\\d*)";
+  private static final String semVerPrerelease =
+      "(?:-((?:0|[1-9]\\d*|\\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\\.(?:0|[1-9]\\d*|\\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?";
+  private static final String semVerBuildmetadata = "(?:\\+([0-9a-zA-Z-]+(?:\\.[0-9a-zA-Z-]+)*))?";
+  private static final Pattern regexpSemVer =
+      Pattern.compile("^" + semVerCore + semVerPrerelease + semVerBuildmetadata + "$");
+
+  private static final Pattern regexpAlphanum = Pattern.compile("^[0-9A-Za-z_\\.\\+-]+$");
+  private static final Pattern regexpAlphanumInverse = Pattern.compile("[^0-9A-Za-z_\\.\\+-]");
+
+  /**
+   * Replaces all non-alphanumeric characters with a hyphen. Use this to ensure that the user agent
+   * value is valid. This is useful when the value is not ensured to be valid at compile time.
+   *
+   * <p>Note: Semver strings are comprised of alphanumeric characters, hyphens, periods and plus
+   * signs. This function will not remove these characters.
+   *
+   * @param s The string to sanitize.
+   * @return The sanitized string.
+   */
+  public static String sanitize(String s) {
+    return regexpAlphanumInverse.matcher(s).replaceAll("-");
+  }
+
+  public static boolean matchSemVer(String s) throws IllegalArgumentException {
+    if (regexpSemVer.matcher(s).matches()) {
+      return true;
+    }
+    throw new IllegalArgumentException("Invalid semver string: " + s);
+  }
+
+  public static boolean matchAlphanum(String s) throws IllegalArgumentException {
+    if (regexpAlphanum.matcher(s).matches()) {
+      return true;
+    }
+    throw new IllegalArgumentException("Invalid alphanumeric string: " + s);
+  }
+
+  public static boolean matchAlphanumOrSemVer(String s) throws IllegalArgumentException {
+    if (regexpAlphanum.matcher(s).matches() || regexpSemVer.matcher(s).matches()) {
+      return true;
+    }
+    throw new IllegalArgumentException("Invalid alphanumeric or semver string: " + s);
+  }
+
+  public static void withPartner(String partner) {
+    withOtherInfo("partner", partner);
+  }
+
   public static void withOtherInfo(String key, String value) {
-    otherInfo.put(key, value);
+    matchAlphanum(key);
+    matchAlphanumOrSemVer(value);
+    otherInfo.add(new Info(key, value));
   }
 
   private static String osName() {
@@ -43,12 +114,15 @@ public class UserAgent {
   }
 
   public static String asString() {
-    String otherInfo =
-        UserAgent.otherInfo.entrySet().stream()
-            .map(e -> String.format(" %s/%s", e.getKey(), e.getValue()))
-            .collect(Collectors.joining());
-    return String.format(
-        "%s/%s databricks-sdk-java/%s jvm/%s os/%s%s",
-        product, productVersion, version, jvmVersion(), osName(), otherInfo);
+    List<String> segments = new ArrayList<>();
+    segments.add(String.format("%s/%s", product, productVersion));
+    segments.add(String.format("databricks-sdk-java/%s", version));
+    segments.add(String.format("jvm/%s", jvmVersion()));
+    segments.add(String.format("os/%s", osName()));
+    segments.addAll(
+        otherInfo.stream()
+            .map(e -> String.format("%s/%s", e.getKey(), e.getValue()))
+            .collect(Collectors.toSet()));
+    return segments.stream().collect(Collectors.joining(" "));
   }
 }
