2f13820d164483e269137ba6af00d9db31ed769b
Renaud Hartert <renaud.hartert@databricks.com>
2024-07-04 17:36:02 +0200

    Fix formatting

--- databricks-sdk-java/src/main/java/com/databricks/sdk/core/commons/CommonsHttpClient.java
+++ databricks-sdk-java/src/main/java/com/databricks/sdk/core/commons/CommonsHttpClient.java
@@ -30,7 +30,6 @@ import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
-import org.apache.http.message.BasicHttpRequest;
 import org.apache.http.protocol.BasicHttpContext;
 import org.apache.http.protocol.HttpContext;
 import org.slf4j.Logger;
@@ -105,14 +104,15 @@ public class CommonsHttpClient implements HttpClient {
       HttpHost targetHost = (HttpHost) context.getAttribute("http.target_host");
       HttpRequest request = (HttpRequest) context.getAttribute("http.request");
       URI uri = new URI(request.getRequestLine().getUri());
-      uri = new URI(
-          targetHost.getSchemeName(),
-          null,
-          targetHost.getHostName(),
-          targetHost.getPort(),
-          uri.getPath(),
-          uri.getQuery(),
-          uri.getFragment());
+      uri =
+          new URI(
+              targetHost.getSchemeName(),
+              null,
+              targetHost.getHostName(),
+              targetHost.getPort(),
+              uri.getPath(),
+              uri.getQuery(),
+              uri.getFragment());
       return uri.toURL();
     } catch (MalformedURLException | URISyntaxException e) {
       throw new DatabricksException("Unable to get target URL", e);
