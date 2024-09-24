86800f25fb19fe3f8ecfdad5e8713ff7d8ef9ae4
Parth Bansal <parth.bansal@databricks.com>
2024-09-24 11:39:52 +0200

    [Fix] Fix listAccountMetastoreAssignments Integration test (#350)
    ## Changes
    <!-- Summary of your changes that are easy to understand -->
    Currently listAccountMetastoreAssignments test assumes to have a single
    workspace assigned to a metastore. Updated the test to check if the
    expected workspace ID is present in the list of metastore assignments
    instead of asserting equality with the entire list.
    
    ## Tests
    <!-- How is this tested? -->
    Tested Locally

--- databricks-sdk-java/src/test/java/com/databricks/sdk/integration/AccountMetastoreAssignmentsIT.java
+++ databricks-sdk-java/src/test/java/com/databricks/sdk/integration/AccountMetastoreAssignmentsIT.java
@@ -6,7 +6,6 @@ import com.databricks.sdk.integration.framework.EnvContext;
 import com.databricks.sdk.integration.framework.EnvOrSkip;
 import com.databricks.sdk.integration.framework.EnvTest;
 import com.databricks.sdk.service.catalog.AccountsMetastoreAssignment;
-import java.util.Collections;
 import java.util.List;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -33,7 +32,6 @@ public class AccountMetastoreAssignmentsIT {
       @EnvOrSkip("TEST_WORKSPACE_ID") String expectedWorkspaceId) {
     Iterable<Long> list = a.metastoreAssignments().list(metastoreId);
     List<Long> all = CollectionUtils.asList(list);
-    List<Long> expected = Collections.singletonList(Long.valueOf(expectedWorkspaceId));
-    Assertions.assertEquals(expected, all);
+    Assertions.assertTrue(all.contains(Long.valueOf(expectedWorkspaceId)));
   }
 }
