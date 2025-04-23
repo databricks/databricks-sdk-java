cf96f3abf306108bfccf3fc33ebbb532d91ec327
emmyzhou-db <emmy.zhou@databricks.com>
2025-04-23 15:50:47 +0000

    Add unit tests

---
+++ databricks-sdk-java/src/test/java/com/databricks/sdk/core/oauth/IDTokenTest.java
@@ -0,0 +1,15 @@
+package com.databricks.sdk.core.oauth;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class IDTokenTest {
+
+    private static final String accessToken = "testIdToken";
+
+    @Test
+    void createIDToken() {
+        IDToken idToken = new IDToken(accessToken);
+        assertEquals(accessToken, idToken.getValue());
+    }
+}
