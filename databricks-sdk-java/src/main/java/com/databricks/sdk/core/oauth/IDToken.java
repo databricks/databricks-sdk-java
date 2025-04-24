33cbee9d70d53059f90635876c004a5b68d1c796
emmyzhou-db <emmy.zhou@databricks.com>
2025-04-24 15:02:33 +0200

    Add IDTokenSource Interface (#433)
    ## What changes are proposed in this pull request?
    This PR adds the foundational components for OIDC support in the
    Databricks Java SDK:
    
    **IDToken Class:** Encapsulates ID tokens from identity providers for
    OAuth flows.
    **IDTokenSource** Interface: Defines a standard for retrieving ID
    tokens.
    
    ## How is this tested?
    - Unit tests added for the IDToken class.
    - IDTokenSource is not tested, as no implementation exists yet.
    
    NO_CHANGELOG=true
    
    ---------
    
    Co-authored-by: Parth Bansal <parth.bansal@databricks.com>

---
+++ databricks-sdk-java/src/main/java/com/databricks/sdk/core/oauth/IDToken.java
@@ -0,0 +1,31 @@
+package com.databricks.sdk.core.oauth;
+
+/**
+ * Represents an ID Token provided by an identity provider from an OAuth flow. This token can later
+ * be exchanged for an access token.
+ */
+public class IDToken {
+  // The string value of the ID Token
+  private final String value;
+
+  /**
+   * Constructs an IDToken with a value.
+   *
+   * @param value The ID Token string.
+   */
+  public IDToken(String value) {
+    if (value == null || value.isEmpty()) {
+      throw new IllegalArgumentException("ID Token value cannot be null or empty");
+    }
+    this.value = value;
+  }
+
+  /**
+   * Returns the value of the ID Token.
+   *
+   * @return The string representation of the ID Token.
+   */
+  public String getValue() {
+    return value;
+  }
+}
