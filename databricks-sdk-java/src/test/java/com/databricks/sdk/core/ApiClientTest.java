187ac13fe953052914e17b775d2688a4e2aa9676
Ubuntu <renaud.hartert@databricks.com>
2025-08-12 16:59:20 +0000

    Fix test

--- databricks-sdk-java/src/test/java/com/databricks/sdk/core/ApiClientTest.java
+++ databricks-sdk-java/src/test/java/com/databricks/sdk/core/ApiClientTest.java
@@ -324,7 +324,7 @@ public class ApiClientTest {
     ErrorDetails errorDetails =
         ErrorDetails.builder()
             .setErrorInfo(
-                ErrorInfo.builder().reason("reason").domain("domain").metadata(metadata).build())
+                ErrorInfo.builder().setReason("reason").setDomain("domain").setMetadata(metadata).build())
             .setUnknownDetails(
                 Arrays.asList(
                     mapper
