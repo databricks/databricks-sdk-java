ac9f21c1e485d08a42728007595227cd2f1d746b
Parth Bansal <parth.bansal@databricks.com>
2025-08-28 08:02:42 +0000

    Enable Encryption Keys test

--- databricks-sdk-java/src/test/java/com/databricks/sdk/integration/EncryptionKeysIT.java
+++ databricks-sdk-java/src/test/java/com/databricks/sdk/integration/EncryptionKeysIT.java
@@ -1,23 +1,23 @@
 package com.databricks.sdk.integration;
 
+import com.databricks.sdk.AccountClient;
+import com.databricks.sdk.integration.framework.CollectionUtils;
 import com.databricks.sdk.integration.framework.EnvContext;
 import com.databricks.sdk.integration.framework.EnvTest;
-import org.junit.jupiter.api.condition.DisabledIfEnvironmentVariable;
+import com.databricks.sdk.service.provisioning.CustomerManagedKey;
+import org.junit.jupiter.api.Test;import org.junit.jupiter.api.condition.DisabledIfEnvironmentVariable;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 @EnvContext("account")
 @DisabledIfEnvironmentVariable(named = "ARM_CLIENT_ID", matches = ".*")
 @ExtendWith(EnvTest.class)
 public class EncryptionKeysIT {
-  // TODO: Enable this test when the test account is updated to support this.
-  // Either by upgrading the test account tier to Enterprise or by adding this
-  // feature to the test account.
-  // @Test
-  // void lists(AccountClient a) {
-  //   Iterable<CustomerManagedKey> list = a.encryptionKeys().list();
+  @Test
+  void lists(AccountClient a) {
+    Iterable<CustomerManagedKey> list = a.encryptionKeys().list();
 
-  //   java.util.List<CustomerManagedKey> all = CollectionUtils.asList(list);
+    java.util.List<CustomerManagedKey> all = CollectionUtils.asList(list);
 
-  //   CollectionUtils.assertUnique(all);
-  // }
+    CollectionUtils.assertUnique(all);
+  }
 }
