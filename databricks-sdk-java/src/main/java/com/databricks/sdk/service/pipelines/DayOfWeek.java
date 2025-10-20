dbe7fbc7026aa83c971fc5c1834725cbc29a010c
Hector Castejon Diaz <hector.castejon@databricks.com>
2025-10-20 12:36:12 +0000

    before

--- databricks-sdk-java/src/main/java/com/databricks/sdk/service/pipelines/DayOfWeek.java
+++ databricks-sdk-java/src/main/java/com/databricks/sdk/service/pipelines/DayOfWeek.java
@@ -5,8 +5,8 @@ package com.databricks.sdk.service.pipelines;
 import com.databricks.sdk.support.Generated;
 
 /**
- * Days of week in which the restart is allowed to happen (within a five-hour window starting at
- * start_hour). If not specified all days of the week will be used.
+ * Days of week in which the window is allowed to happen. If not specified all days of the week will
+ * be used.
  */
 @Generated
 public enum DayOfWeek {
