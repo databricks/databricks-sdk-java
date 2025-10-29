eddeaa59eeb5ff7694b01424b4a73e4cd1c65775
Renaud Hartert <renaud.hartert@databricks.com>
2025-10-29 14:29:18 +0100

    Clarify behavior of CredentialsProvider. (#537)
    ## What changes are proposed in this pull request?
    
    This PR clarifies behavior and expectation for method
    `CredentialsProvider.configure()` to return a new `HeaderFactory`
    instance on each invocation.
    
    ## How is this tested?
    
    N/A

--- NEXT_CHANGELOG.md
+++ NEXT_CHANGELOG.md
@@ -10,6 +10,8 @@
 
 ### Documentation
 
+* Clarify behavior and expectation for method `CredentialsProvider.configure()` to return a new `HeaderFactory` instance on each invocation.
+
 ### Internal Changes
 
 ### API Changes
--- databricks-sdk-java/src/main/java/com/databricks/sdk/core/CredentialsProvider.java
+++ databricks-sdk-java/src/main/java/com/databricks/sdk/core/CredentialsProvider.java
@@ -1,7 +1,24 @@
 package com.databricks.sdk.core;
 
+/**
+ * CredentialsProvider is an interface that provides a HeaderFactory to authenticate requests to the
+ * Databricks API.
+ */
 public interface CredentialsProvider {
+  /**
+   * Returns the authentication type identifier for this credentials provider.
+   *
+   * @return the authentication type as a string
+   */
   String authType();
 
+  /**
+   * Creates and returns a new HeaderFactory to authenticate requests to the Databricks API.
+   *
+   * <p>Note: A new HeaderFactory instance is returned on each invocation.
+   *
+   * @param config the Databricks configuration to use for authentication
+   * @return a new HeaderFactory configured for authenticating API requests
+   */
   HeaderFactory configure(DatabricksConfig config);
 }
