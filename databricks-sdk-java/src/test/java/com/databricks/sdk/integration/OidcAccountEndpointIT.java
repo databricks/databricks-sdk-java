3ddedf1c77df81a9582dbde9e5aacd4273e9523c
Hector Castejon Diaz <hector.castejon@databricks.com>
2026-02-09 15:30:33 +0000

    more tests

--- databricks-sdk-java/src/test/java/com/databricks/sdk/integration/OidcAccountEndpointIT.java
+++ databricks-sdk-java/src/test/java/com/databricks/sdk/integration/OidcAccountEndpointIT.java
@@ -1,18 +1,20 @@
 package com.databricks.sdk.integration;
 
 import com.databricks.sdk.AccountClient;
+import com.databricks.sdk.core.DatabricksConfig;
 import com.databricks.sdk.core.oauth.OpenIDConnectEndpoints;
 import com.databricks.sdk.integration.framework.EnvContext;
 import com.databricks.sdk.integration.framework.EnvTest;
 import java.io.IOException;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 @EnvContext("account")
 @ExtendWith(EnvTest.class)
 public class OidcAccountEndpointIT {
   @Test
-  void lists(AccountClient a) throws IOException {
+  void checkEndpoints(AccountClient a) throws IOException {
     OpenIDConnectEndpoints endpoints = a.config().getOidcEndpoints();
     String host = a.config().getHost();
     String accountId = a.config().getAccountId();
@@ -27,5 +29,34 @@ public class OidcAccountEndpointIT {
           .getTokenEndpoint()
           .equals(host + "/oidc/accounts/" + accountId + "/v1/token");
     }
+  }
+
+  @Test
+  void unifiedEndpoints(AccountClient a) throws IOException {
+    DatabricksConfig cfg = a.config();
+    cfg.setExperimentalIsUnifiedHost(true);
+    OpenIDConnectEndpoints endpoints = cfg.getOidcEndpoints();
+    String host = cfg.getHost();
+    String accountId = cfg.getAccountId();
+    assert endpoints
+        .getAuthorizationEndpoint()
+        .equals(host + "/oidc/accounts/" + accountId + "/v1/authorize");
+    assert endpoints.getTokenEndpoint().equals(host + "/oidc/accounts/" + accountId + "/v1/token");
+  }
+
+  @Test
+  @EnabledIfEnvironmentVariable(named = "UNIFIED_HOST", matches = ".+")
+  void unifiedEndpointsForSpog(AccountClient a) throws IOException {
+    String unifiedHost = System.getenv("UNIFIED_HOST");
+    DatabricksConfig cfg = a.config();
+    cfg.setHost(unifiedHost);
+    cfg.setExperimentalIsUnifiedHost(true);
+    OpenIDConnectEndpoints endpoints = cfg.getOidcEndpoints();
+    String host = cfg.getHost();
+    String accountId = cfg.getAccountId();
+    assert endpoints
+        .getAuthorizationEndpoint()
+        .equals(host + "/oidc/accounts/" + accountId + "/v1/authorize");
+    assert endpoints.getTokenEndpoint().equals(host + "/oidc/accounts/" + accountId + "/v1/token");
   }
 }
--- databricks-sdk-java/src/test/java/com/databricks/sdk/integration/OidcUcWorkspaceEndpointIT.java
+++ databricks-sdk-java/src/test/java/com/databricks/sdk/integration/OidcUcWorkspaceEndpointIT.java
@@ -1,21 +1,39 @@
 package com.databricks.sdk.integration;
 
 import com.databricks.sdk.WorkspaceClient;
+import com.databricks.sdk.core.DatabricksConfig;
 import com.databricks.sdk.core.oauth.OpenIDConnectEndpoints;
 import com.databricks.sdk.integration.framework.EnvContext;
 import com.databricks.sdk.integration.framework.EnvTest;
 import java.io.IOException;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 @EnvContext("ucws")
 @ExtendWith(EnvTest.class)
 public class OidcUcWorkspaceEndpointIT {
   @Test
-  void lists(WorkspaceClient w) throws IOException {
+  void checkEndpoints(WorkspaceClient w) throws IOException {
     OpenIDConnectEndpoints endpoints = w.config().getOidcEndpoints();
     String host = w.config().getHost();
     assert endpoints.getAuthorizationEndpoint().equals(host + "/oidc/v1/authorize");
     assert endpoints.getTokenEndpoint().equals(host + "/oidc/v1/token");
+  }
+
+  @Test
+  @EnabledIfEnvironmentVariable(named = "UNIFIED_HOST", matches = ".+")
+  void unifiedEndpointsForSpog(WorkspaceClient c) throws IOException {
+    String unifiedHost = System.getenv("UNIFIED_HOST");
+    DatabricksConfig cfg = c.config();
+    cfg.setHost(unifiedHost);
+    cfg.setExperimentalIsUnifiedHost(true);
+    OpenIDConnectEndpoints endpoints = cfg.getOidcEndpoints();
+    String host = cfg.getHost();
+    String accountId = cfg.getAccountId();
+    assert endpoints
+        .getAuthorizationEndpoint()
+        .equals(host + "/oidc/accounts/" + accountId + "/v1/authorize");
+    assert endpoints.getTokenEndpoint().equals(host + "/oidc/accounts/" + accountId + "/v1/token");
   }
 }
