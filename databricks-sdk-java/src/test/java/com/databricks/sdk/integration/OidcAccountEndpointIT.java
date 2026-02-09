b5a1b7aaa720a5bee5568b6b029537b33c09f3ab
Hector Castejon Diaz <hector.castejon@databricks.com>
2026-02-09 14:47:48 +0000

    add oidc endpoint test

---
+++ databricks-sdk-java/src/test/java/com/databricks/sdk/integration/OidcAccountEndpointIT.java
@@ -0,0 +1,31 @@
+package com.databricks.sdk.integration;
+
+import com.databricks.sdk.AccountClient;
+import com.databricks.sdk.core.oauth.OpenIDConnectEndpoints;
+import com.databricks.sdk.integration.framework.EnvContext;
+import com.databricks.sdk.integration.framework.EnvTest;
+
+import java.io.IOException;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+
+@EnvContext("account")
+@ExtendWith(EnvTest.class)
+public class OidcAccountEndpointIT {
+  @Test
+  void lists(AccountClient a) throws IOException {
+    OpenIDConnectEndpoints endpoints = a.config().getOidcEndpoints();
+    String host = a.config().getHost();
+    String accountId = a.config().getAccountId();
+    if (a.config().isAzure()) {
+      assert endpoints.getAuthorizationEndpoint().equals(host + "/oidc/v1/authorize");
+      assert endpoints.getTokenEndpoint().equals(host + "/oidc/v1/token");
+    } else {
+      assert endpoints.getAuthorizationEndpoint().equals(host + "/oidc/accounts/" + accountId + "/v1/authorize");
+      assert endpoints.getTokenEndpoint().equals(host + "/oidc/accounts/" + accountId + "/v1/token");
+    }
+  }
+
+  
+}
---
+++ databricks-sdk-java/src/test/java/com/databricks/sdk/integration/OidcWorkspaceEndpointIT.java
@@ -0,0 +1,25 @@
+package com.databricks.sdk.integration;
+
+import com.databricks.sdk.WorkspaceClient;
+import com.databricks.sdk.core.oauth.OpenIDConnectEndpoints;
+import com.databricks.sdk.integration.framework.EnvContext;
+import com.databricks.sdk.integration.framework.EnvTest;
+
+import java.io.IOException;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+
+@EnvContext("workspace")
+@ExtendWith(EnvTest.class)
+public class OidcWorkspaceEndpointIT {
+  @Test
+  void lists(WorkspaceClient w) throws IOException {
+    OpenIDConnectEndpoints endpoints = w.config().getOidcEndpoints();
+    String host = w.config().getHost();
+    assert endpoints.getAuthorizationEndpoint().equals(host + "/oidc/v1/authorize");
+    assert endpoints.getTokenEndpoint().equals(host + "/oidc/v1/token");
+  }
+
+  
+}
